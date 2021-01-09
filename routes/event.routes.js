77cca197d9079d34a920c56895391af2bdc436a4
“camillebrown” <camillebrown11@gmail.com>
2021-01-09 08:05:54 -0800

    updated backend routes with auth

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -4,10 +4,10 @@ const { authJwt } = require('../middlewares')
 
 module.exports = function(app){
     // routes for events
-    app.get("/profile/myevents",[authJwt.verifyWebToken], controller.seeEvents) //NAOMI
+    app.get("/profile/myevents", [authJwt.verifyWebToken], controller.seeEvents) //NAOMI
     app.delete("/profile/myevents/:id", controller.deleteEvent) // PREET
     app.post("/profile/myevents/addevent",[authJwt.verifyWebToken], controller.saveEvent) // CAMILLE
-    app.delete("/profile/calendar", [authJwt.verifyWebToken], controller.deleteEvent) // PREET
+    app.delete("/profile/calendar", controller.deleteEvent) // PREET
 
     // routes for comments work
     app.get("/events/comments/:id", [authJwt.verifyWebToken],controller.seeComments) // KRYSTLE
