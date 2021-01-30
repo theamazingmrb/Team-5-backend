34d12d21f72041278fc90bf822f0ea58fa8910ad
ktalens <ktal66@yahoo.com>
2021-01-30 14:52:19 -0800

    cors error still- changed use cors origin true

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const dbConfig = require('./config/db.config')
 const cors = require("cors")
 
 const app = express();
-app.use(cors({origin:true}))
+app.use(cors())
 require('dotenv').config();
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://fomo-nomo-frontend.surge.sh');
-    // res.header(
-    //     "Access-Control-Allow-Headers",
-    //     "x-access-token, Origin, Content-type, Accept"
-    // )
+    //res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        "Access-Control-Allow-Headers",
+        "x-access-token, Origin, Content-type, Accept"
+    )
     next();
   });
 
