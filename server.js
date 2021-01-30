d07cd259ebd9698e61409657a0df3fe03194c7e2
ktalens <ktal66@yahoo.com>
2021-01-30 10:16:52 -0800

    forgot to save

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(cors({origin:true}))
 require('dotenv').config();
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    //res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    //res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, x-requested-with, Content-Type, Accept');
     next();
   });
 
